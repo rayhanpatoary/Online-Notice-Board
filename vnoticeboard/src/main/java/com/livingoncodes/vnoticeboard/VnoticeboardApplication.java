202a60dc73f4ce0cdf4c1d2bfb1bddb2186ed143
Rayhanpatoary <mailforfindme@gmail.com>
2020-07-04 07:34:28 +0200

    initial Commit

---
+++ vnoticeboard/src/main/java/com/livingoncodes/vnoticeboard/VnoticeboardApplication.java
@@ -0,0 +1,13 @@
+package com.livingoncodes.vnoticeboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VnoticeboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VnoticeboardApplication.class, args);
+	}
+
+}
---
+++ vnoticeboard/src/test/java/com/livingoncodes/vnoticeboard/VnoticeboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.livingoncodes.vnoticeboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VnoticeboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
